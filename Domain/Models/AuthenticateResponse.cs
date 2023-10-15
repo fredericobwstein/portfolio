389bff5701140e4013051eb8196224da55128ab9
Fred <fredberstein@gmail.com>
2023-10-15 03:08:34 -0300

    Adicionar arquivos de projeto.

---
+++ Domain/Models/AuthenticateResponse.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Entidades;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class AuthenticateResponse
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Token { get; set; }
+
+
+        public AuthenticateResponse(User user, string token)
+        {
+            Id = user.Id;
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+            Username = user.Username;
+            Token = token;
+        }
+    }
+}
